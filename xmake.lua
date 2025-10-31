af8cd24f3264753fed6fd76d44afcca39fabc350
ZXPrism <2897544036@qq.com>
2025-10-31 09:23:14 +0800

    init

---
+++ xmake.lua
@@ -0,0 +1,15 @@
+set_project("cuda-lab")
+
+add_rules("mode.debug", "mode.release")
+
+target("radix_sort")
+    set_languages("cxx20")
+    set_kind("binary")
+    set_warnings("all", "extra", "pedantic")
+
+    add_files("src/radix_sort.cu")
+
+    after_build(function (target)
+        os.cp(target:targetfile(), "bin/")
+    end)
+target_end()
